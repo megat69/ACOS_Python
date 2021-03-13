fbf99529eab2e75360887ffd23da6e7d7c8621e0
TheAssassin <47896737+megat69@users.noreply.github.com>
2021-03-13 13:52:28 +0100

    Alpha0.14

--- ROOT/softwares/software_api.py
+++ ROOT/softwares/software_api.py
@@ -2,6 +2,7 @@
 A bunch of APIs for the ACOS sotwares.
 """
 import json
+import tkinter as tk
 
 # The registry
 try:
@@ -23,16 +24,11 @@ os_version = general_data["version"]
 
 general_data_file.close()
 
-def api_params(function):
-	"""
-	Decorator function, used to give the app access a
-	read-only registry and the current username.
-	"""
-	global REGISTRY
-	global current_user
-	def wrapper(*args, **kwargs):
-		return function(*args, **kwargs)
-	return wrapper
+__window = None
+
+def __set_window(win):
+	global __window
+	__window = win
 
 def refresh_registry():
 	global REGISTRY
@@ -44,4 +40,51 @@ def refresh_registry():
 	registry_file.close()
 
 def notify(title:str, text:str):
-	pass
+	"""
+	Creates a notification of the user desktop.
+	"""
+	global __window
+
+	temp_text = [text[i:i+22] for i in range(0, len(text), 22)]
+	text = ""
+	for i in range(len(temp_text)):
+		temp_text[i] += "\n"
+		text += temp_text[i]
+	del temp_text
+
+	globals()["notification"] = tk.Frame(
+		__window
+	)
+	notification_title = tk.Label(
+		globals()["notification"],
+		text = title,
+		font = ("Impact", 16)
+	)
+	notification_title.pack()
+	notification_message = tk.Label(
+		globals()["notification"],
+		text = text,
+		font = ("Calibri", 12)
+	)
+	notification_message.pack()
+
+	height = 120
+	width = 240
+
+	globals()["notification"].place(
+		width = width,
+		height = height,
+		x = __window.winfo_width() - 10 - width,
+		y = __window.winfo_height() - 10 - height
+	)
+
+	def destroy_notification(notification:tk.Frame):
+		notification.place_forget()
+		notification.destroy()
+
+	try:
+		__window.after(REGISTRY["NOTIFICATION_STAYING_TIME"], destroy_notification, globals()["notification"])
+	except:
+		from ...main import ThrowBSOD, corrupted_key
+		ThrowBSOD(__window, corrupted_key("NOTIFICATION_STAYING_TIME"))
+
