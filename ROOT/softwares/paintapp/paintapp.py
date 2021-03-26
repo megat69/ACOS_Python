ca420143a78f50744f852da687c244eabaf63a60
TheAssassin <47896737+megat69@users.noreply.github.com>
2021-03-26 18:53:20 +0100

    Pre1.0_3

--- ROOT/softwares/paintapp/paintapp.py
+++ ROOT/softwares/paintapp/paintapp.py
@@ -4,10 +4,6 @@ from PIL import Image, EpsImagePlugin
 from functools import partial
 import os
 import json
-import requests
-import zipfile
-import shutil
-import asyncio
 
 app_icon = "Paintapp_Logo.png"
 software_name = "Paintapp"
@@ -28,50 +24,6 @@ dot_size_var.set(dot_size)
 
 nbr_imported_images = 0
 
-async def download_repo(URL:str, folder:str, files_to_delete:(list, tuple)=(".gitattributes", "README.md"), log:bool=True, notify_function=print, *args, **kwargs):
-	"""
-	Downloads the specified repository into the specified folder.
-	:param URL: The URL of the repository main archive.
-	:param folder: The folder to place the repository in.
-	:param files_to_delete: The files to delete after downloading the repository. DEFAULT : '.gitattributes', 'README.md'
-	:param log: Log the informations in the console. DEFAULT : True
-	"""
-	if log: notify_function("Downloading... This might take a while.", *args, **kwargs)
-	r = requests.get(URL)
-	assert r.status_code == 200, "Something happened.\nStatus code : " + str(r.status_code)
-
-	if log: notify_function("Writing the zip...", *args, **kwargs)
-	# Writing the zip
-	with open(f"{folder}.zip", "wb") as code:
-		code.write(r.content)
-		code.close()
-
-	# Creating a folder for the zip content
-	if not os.path.exists(folder):
-		os.mkdir(folder)
-
-	if log: notify_function("Extracting...", *args, **kwargs)
-	# Extracting the zip
-	with zipfile.ZipFile(f"{folder}.zip", "r") as zip_ref:
-		zip_ref.extractall(folder)
-
-	if log: notify_function("Moving the files...", *args, **kwargs)
-	suffix = URL.split("/")[-1].replace(".zip", "", 1)
-	repo_name = URL.split("/")[4]
-	# Moving the file to parent
-	for filename in os.listdir(os.path.join(folder, f'{repo_name}-{suffix}')):
-		shutil.move(os.path.join(folder, f'{repo_name}-{suffix}', filename), os.path.join(folder, filename))
-	# Deleting unnecessary files
-	shutil.rmtree(f"{folder}/{repo_name}-{suffix}")
-	os.remove(f"{folder}.zip")
-	for file in files_to_delete:
-		try:
-			os.remove(f"{folder}/{file}")
-		except FileNotFoundError:
-			pass
-
-	if log: notify_function("Download complete !", *args, **kwargs)
-
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 # Translations
 if software_api.REGISTRY["SYSTEM_LANG"].lower() == "fr":
@@ -80,15 +32,6 @@ else:
 	translation_file = open("translations_en.json", "r")
 TRANSLATIONS = json.load(translation_file)
 translation_file.close()
-
-# Installing GhostScript if not installed
-if not os.path.exists("gs9.53.3/"):
-	software_api.notify(software_name, "Downloading GhostScript...")
-
-	def notifier(text):
-		software_api.notify(software_name, text)
-
-	asyncio.run(download_repo("https://github.com/megat69/GhostScript/archive/refs/heads/main.zip", "gs.53.3", notify_function=notifier))
 
 os.chdir("../../../")
 
